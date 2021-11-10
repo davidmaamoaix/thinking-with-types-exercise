02c47440da727ad21ee4c91acf38f564d4df058e
BuildTools <davidmaamoaix@gmail.com>
2021-11-10 21:02:23 +0800

    ST trick

--- 2_4-existential-types.hs
+++ 2_4-existential-types.hs
@@ -5,13 +5,16 @@
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module ExistentialTypes where
 
 import Data.Kind
 import Data.Maybe
+import Data.IORef
 import Data.Typeable
 import Control.Applicative
+import System.IO.Unsafe (unsafePerformIO)
 
 type Dynamic = Has Typeable
 
@@ -44,3 +47,44 @@ pyPlus a b = fromMaybe (Has $ Error "bad types") $ liftD2 @String @String (++) a
                                                    <|> liftD2 @Int @Int (+) a b
                                                    <|> liftD2 @String @Int ((. show) . (++)) a b
                                                    <|> liftD2 @Int @String ((++) . show) a b
+
+class (Monoid a, Eq a) => MonoidEq a
+instance (Monoid a, Eq a) => MonoidEq a
+
+-- ST implementation
+newtype ST s a = ST { unsafeRunST :: a }
+
+instance Functor (ST s) where
+    fmap f (ST a) = seq a . ST $ f a
+
+instance Applicative (ST s) where
+    pure = ST
+    ST f <*> ST a = seq f . seq a . ST $ f a
+
+instance Monad (ST s) where
+    ST a >>= f = seq a $ f a
+
+newtype STRef s a = STRef { unSTRef :: IORef a }
+
+newSTRef :: a -> ST s (STRef s a)
+newSTRef = pure . STRef . unsafePerformIO . newIORef
+
+readSTRef :: STRef s a -> ST s a
+readSTRef = pure . unsafePerformIO . readIORef . unSTRef
+
+writeSTRef :: STRef s a -> a -> ST s ()
+writeSTRef = ((pure . unsafePerformIO) .) . writeIORef . unSTRef
+
+modifySTRef :: STRef s a -> (a -> a) -> ST s ()
+modifySTRef ref f = do
+    a <- readSTRef ref
+    writeSTRef ref (f a)
+
+runST :: (forall s. ST s a) -> a
+runST = unsafeRunST
+
+safeExample :: ST s String
+safeExample = do
+    ref <- newSTRef "Hello"
+    modifySTRef ref (++ " World")
+    readSTRef ref
